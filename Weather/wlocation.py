1e7d8d69c4b71dffd9c80e45f0c0a71242c53d20
KaustubhDamania <kaustubh.damania@gmail.com>
2019-03-10 21:10:31 +0530

    Removed unused imports/code

--- Weather/wlocation.py
+++ Weather/wlocation.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 from flask_simple_geoip import SimpleGeoIP
-import socket
 import requests
 
 app = Flask(__name__)
@@ -8,10 +7,9 @@ app.debug = True
 
 @app.route('/')
 def location():
-    IP = socket.gethostbyname(socket.gethostname())
     data = requests.get('https://geoipify.whoisxmlapi.com/api/v1?apiKey=at_tO6ReNldmeViKxs7xF6Vo8bwgU1tG&ipAddress=').json()
     city = data['location']['city']
-    
+
     req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ city +'&units=celsius&appid=271d1234d3f497eed5b1d80a07b3fcd1').json()
     description = req['weather'][0]['description'].upper()
     temperature = req['main']['temp'] - 273.15
